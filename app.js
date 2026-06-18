c8dc1d6c9f766675722152e3e795044e099d17e5
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-18 17:44:29 +0000

    Change service worker registration file to sw.js

--- app.js
+++ app.js
@@ -2049,7 +2049,7 @@ updateShuffleButton();
 
 if ("serviceWorker" in navigator) {
 
-  navigator.serviceWorker.register("./sw-beta.js")
+  navigator.serviceWorker.register("./sw.js")
     .then((registration) => {
 
       console.log("Service Worker registered");
