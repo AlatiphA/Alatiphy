f4ec2b70e3bd3bb03fbf3ad356bb30cdf053d8d6
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-08-01 15:24:20 +0000

    Update Font Awesome CSS version in service worker

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ const ASSETS = [
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css",
 ];
 
 /* ====================
